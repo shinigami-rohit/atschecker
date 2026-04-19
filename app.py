dac19a12f33c0a9ae417f2d6a45d1060f05a1500
rohit birua <rohitbirua593@gmail.com>
2026-04-19 14:38:53 +0530

    Add test route to Flask application

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "ATS Checker is running successfully 🚀"
+
+@app.route("/test", methods=["GET"])
+def test():
+    return jsonify({"message": "API working ✅"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
